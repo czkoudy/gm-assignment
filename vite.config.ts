28ba4720f6c00eef6ecb70dbe58d6492e1e50ead
czkoudy <czkoudy@gmail.com>
2023-07-28 08:23:50 +0200

    Changed vite.config settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/czkoudy/gm-assignment/',
+  base: '/gm-assignment',
   plugins: [react()],
   resolve: {
     alias: {
